add0b31de18806c28e55db290505926f48618777
Virendra <90092655+Virendra85@users.noreply.github.com>
2022-09-20 14:36:04 +0530

    Update settings.py

--- LIBRARY/LIBRARY/settings.py
+++ LIBRARY/LIBRARY/settings.py
@@ -83,8 +83,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'virendravishwakarma966@gmail.com'
-EMAIL_HOST_PASSWORD = 'giyifzbtflzdvpoy'
+EMAIL_HOST_USER = 'realvren@gmail.com'
+EMAIL_HOST_PASSWORD = ''
 
 
 # Database
